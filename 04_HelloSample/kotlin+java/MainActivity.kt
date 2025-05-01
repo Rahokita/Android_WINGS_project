881bffedfe585227214af3c5c73e53388464fdac
Rahokita <47273613+Rahokita@users.noreply.github.com>
2025-05-01 15:40:44 +0900

    List 4.5 and 4.6 :Update MainActivity.kt

--- 04_HelloSample/kotlin+java/MainActivity.kt
+++ 04_HelloSample/kotlin+java/MainActivity.kt
@@ -1,6 +1,32 @@
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // 表示ボタンであるButtonオブジェクトを取得
+        val btClick = findViewById<Button>(R.id.btClick)
+
+        // リスナクラスのインスタンスを生成
+        val linstener = HelloListener()
+
+        // 表示ボタンにリスナを設定
+        btClick.setOnClickListener(linstener)
+    }
+    // ボタンをクリックしたときのリスナクラス
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            // 名前入力欄であるEditTextオブジェクトを取得
+            val input = findViewById<EditText>(R.id.etName)
+
+            //　メッセージを表示するTextViewオブジェクトを取得
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            // 入力された名前文字列を取得
+            val inputStr = input.text.toString()
+
+            // メッセージを表示
+            output.text = inputStr + "さん、こんにちは！"
+        }
     }
 }
