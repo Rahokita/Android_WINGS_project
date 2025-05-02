b84d51bc7a97d40e40e9d3c0f7b898720584188c
Rahokita <47273613+Rahokita@users.noreply.github.com>
2025-05-02 19:55:36 +0900

    List 5.10 Update MainActivity.kt

--- 05_ListViewSample2/kotlin+java/MainActivity.kt
+++ 05_ListViewSample2/kotlin+java/MainActivity.kt
@@ -1,4 +1,4 @@
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +16,18 @@ class MainActivity : ComponentActivity() {
         val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
         // リストビューにアダプタオブジェクトを設定
         lvMenu.adapter = adapter
+        //リストビューにリスナを設定
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    // リストがタップされたときの処理が記述されたメンバクラス
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            // 注文確認ダイアログフラグメントオブジェクトを生成
+            val dialogFragment = OrderConfirmDialogFragment()
+            // ダイアログ表示
+            dialogFragment.show(supportFragmentManager,"OrderConfirmDialogFragment")
+
+        }
     }
 }
